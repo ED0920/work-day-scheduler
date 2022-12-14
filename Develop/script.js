2e288371adcd94d98c0d229a8fbe43e1acdda422
Elisa <elisa.do@outlook.com>
2022-12-14 18:18:06 +1100

    wrapped for loops in Jquery function

--- Develop/script.js
+++ Develop/script.js
@@ -1,43 +1,11 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 
-// $("iner").wrap(<"div calss");
-
-// $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-// });
-
+$(function(){
 let currentDateEl = document.getElementById("currentDay");
-
-
-// Format date in header to grab current date
 var d = new dayjs().format("dddd, MMMM DD");
-
-// currentDateEl.innerHTML = d
-
-// Save button to save text in text area
-// for (var i = 9; i < 18; i++){
-// var savedText =localStorage.getItem("key"+i);
-// document.getElementById("text"+i).value = savedText
-// }
+const currentD = new Date();
+var currentHour = currentD.getHours();
+console.log(currentHour);
+currentDateEl.innerHTML = d
 
 for (let i = 9; i < 18; i++){
   document.getElementById("saveBtn"+ i).addEventListener("click", function() {
@@ -47,24 +15,7 @@ for (let i = 9; i < 18; i++){
   });
   var savedText = window.localStorage.getItem("key"+i);
   document.getElementById("text"+i).value = savedText;
-
 }
-
-const currentD = new Date();
-var currentHour = currentD.getHours();
-console.log(currentHour);
-
-// for (var i=9; i<18; i++){
-//   if (i < currentHour){
-//     document.getElementById("hour-"+i).style.backgroundColour = 'grey'
-//   }
-//   if (i === currentHour){
-//     document.getElementById("hour-"+i).style.backgroundColour = "red"
-//   }
-//   if (i > currentHour){
-//     document.getElementById("hour-"+i).style.backgroundColour = 'green'
-//   }
-// }
 
 for (var i=9; i<18; i++){
   if (i < currentHour){
@@ -77,3 +28,4 @@ for (var i=9; i<18; i++){
     document.getElementById("hour-"+i).style.backgroundColor = 'green'
   }
 }
+});
